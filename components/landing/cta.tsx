d5425dcb27c8d4a9a14e0e1f69b6ed03ba74141b
Ameya Shrivastava <ameyshri051@gmail.com>
2025-04-29 22:24:08 +0530

    fix: adjust layout and improve readability in CTA and Testimonials components

--- components/landing/cta.tsx
+++ components/landing/cta.tsx
@@ -1,17 +1,21 @@
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function CTA() {
   return (
     <section className="max-w-4xl mx-auto py-20 pb-40">
       <div className="text-center max-w-5xl mx-auto bg-muted py-20 px-10 md:rounded-3xl">
-        <h2 className="text-4xl mb-1 tracking-tighter">Ready to Take Control of Your Finances?</h2>
+        <h2 className="text-4xl mb-1 tracking-tighter">
+          Ready to Take Control of Your Finances?
+        </h2>
         <p className="text-gray-700 dark:text-gray-400 mb-8">
-          Join thousands of users who are already managing their finances smarter with spenz.
+          Join thousands of users who are already managing their finances
+          smarter with spenz.
         </p>
         <Button className="bg-primary text-white px-8 py-6 text-lg hover:opacity-90 transition-opacity">
-          Get Started Now
+          <Link href={"sign-in"}>Get Started Now</Link>
         </Button>
       </div>
     </section>
-  )
+  );
 }
--- components/landing/testimonials.tsx
+++ components/landing/testimonials.tsx
@@ -108,7 +108,7 @@ export default function Testimonials() {
           lives with spenz.
         </p>
       </motion.div>
-      <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden">
+      <div className="relative flex h-[400px] w-full flex-col items-center justify-center overflow-hidden">
         <Marquee pauseOnHover className="[--duration:20s]">
           {firstRow.map((review) => (
             <ReviewCard key={review.username} {...review} />
